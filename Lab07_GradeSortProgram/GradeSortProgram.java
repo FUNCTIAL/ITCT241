7ffc988c5624221e8994c4524686b38e236a34a0
FUNCTIAL <77804731+FUNCTIAL@users.noreply.github.com>
2024-10-30 16:37:32 +0700

    Finish Lab07

---
+++ Lab07_GradeSortProgram/GradeSortProgram.java
@@ -0,0 +1,101 @@
+// File: GradeSortProgram.java
+// Description: Sort student grades using multiple sorting algorithms
+//
+// Assignment Number: 7
+//
+// ID: 6580838
+// Name: Teerathad Udomjitwiwat
+// Grader: Aj.Petch
+//
+// On my honor, Teerathad Udomjitwiwat, this lab assignment is my own work
+// and I have not provided this code to any other students
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class GradeSortProgram {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void selectionSort(int[] arr) {
+        int size = arr.length;
+
+        System.out.println("Starting Selection Sort...");
+
+        for (int i = 0; i < size - 1; i++) {
+            int least = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[least]) {
+                    least = j;
+                }
+            }
+            swap(arr, i, least);
+            System.out.println("Step " + (i + 1) + ": " + Arrays.toString(arr));
+        }
+
+        System.out.println(
+            "Sorted Grades (Selection Sort): " + Arrays.toString(arr) + "\n"
+        );
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int size = arr.length;
+
+        System.out.println("Starting Bubble Sort...");
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+
+            System.out.println("Step " + (i + 1) + ": " + Arrays.toString(arr));
+        }
+
+        System.out.println(
+            "Sorted Grades (Bubble Sort): " + Arrays.toString(arr) + "\n"
+        );
+    }
+
+    public static void insertionSort(int[] arr) {
+        int size = arr.length;
+
+        System.out.println("Starting Insertion Sort...");
+
+        for (int i = 1; i < size; i++) {
+            int unsortedIndex = arr[i];
+            int j = i;
+            while (j > 0 && unsortedIndex < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = unsortedIndex;
+
+            System.out.println("Step " + (i + 1) + ": " + Arrays.toString(arr));
+        }
+
+        System.out.println(
+            "Sorted Grades (Insertion Sort): " + Arrays.toString(arr) + "\n"
+        );
+    }
+
+    public static void main(String[] args) {
+        int[] grade = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < grade.length; i++) grade[i] = random.nextInt(100);
+
+        System.out.println("Original Grades: " + Arrays.toString(grade) + "\n");
+
+        int[] a = new int[]{23, 78, 45, 8, 32, 56};
+
+        selectionSort(a.clone());
+        bubbleSort(a.clone());
+        insertionSort(a.clone());
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 Computational Problem Solving
 
-class should start with capital letter
+[Style Guide](https://google.github.io/styleguide/javaguide.html)
 
 TODO
-class ArrayList
-class LinkedList
-try RecursiveLinkedList
+- class ArrayList
+- class LinkedList
+- class RecursiveLinkedList
+- class DoublyLinkedList
+
+
+## Selection Sort
+20 8 35 12 1 5
+1 8 35 12 20 5
+1 5 35 12 20 8
+1 5 8 12 20 35
+1 5 8 12 20 35
+
+## Bubble Sort
+20 8 35 12 1 5
+8 20 12 1 5 35
+8 12 1 5 20 35
+8 1 5 12 20 35
+8 1 5 12 20 35
+
+## Insertion Sort
+20 | 8 35 12 1 5
+8 20 | 35 12 1 5
+8 20 35 | 12 1 5
+8 12 20 35 | 1 5
+8 12 20 35 | 1 5
+1 8 12 20 35 | 5
+1 8 5 20 12 35 |
